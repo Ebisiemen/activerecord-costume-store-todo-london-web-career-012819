be91a6d0c74f85a9bcaeddd1900b3891a4a854d7
Ebisiemen <ebisiemen@gmail.com>
2020-08-06 03:44:21 +0100

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,15 @@
+class CostumeStore < ActiveRecord::Migration[5.2]
+    def change 
+        create_table :costumes do |t|
+            t.string :name 
+            t.string :location 
+            t.integer :costume_inventory
+            t.integer :num_of_employees
+            t.string:still_in_business
+            t.timestamp :opening_time
+            t.timestamp :closing_time
+        end 
+    end 
+end   
+
 # Create your CostumeStore class here
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change 
+        create_table costumes do |t|
+            t.string :name 
+            t.string :location
+            t.string :theme 
+            t.float :price
+            t.boolean :family_friendly 
+            t.timestamp :opening_date 
+            t.timestamp :closing_date 
+            t.string :description
+        end 
+    end 
+end     
+
+
 # Create your haunted_houses migration here
