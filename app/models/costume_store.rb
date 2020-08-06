14ea952d1cd03dc7b60eb880c6216f665c4d83c7
Ebisiemen <ebisiemen@gmail.com>
2020-08-06 11:55:10 +0100

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,15 +1,4 @@
-class CostumeStore < ActiveRecord::Migration[5.2]
-    def change 
-        create_table :costumes do |t|
-            t.string :name 
-            t.string :location 
-            t.integer :costume_inventory
-            t.integer :num_of_employees
-            t.string:still_in_business
-            t.timestamp :opening_time
-            t.timestamp :closing_time
-        end 
-    end 
+class CostumeStore < ActiveRecord::Base
 end   
 
 # Create your CostumeStore class here
